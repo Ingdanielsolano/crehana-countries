a92cbd5026711604057549b2f142569f856fb605
IngDanielSolano <ds.development.app@gmail.com>
2022-06-05 19:26:01 -0500

    feat(country): clean components

--- components/CountryDetail/CountryDetail.tsx
+++ components/CountryDetail/CountryDetail.tsx
@@ -7,32 +7,30 @@ interface CountryDetailProps {
   country: Country;
 }
 
-const CountryDetail: FC<CountryDetailProps> = ({ country }) => {
-  return (
-    <div className="country-detail">
-      <div className="country-detail__header">
-        <p>
-          <span>{country.name}</span>, {country.capital}
-        </p>
-        <img
-          src={`https://countryflagsapi.com/png/${country.code}`}
-          alt={`Country-${country.name}`}
-        />
-      </div>
-      <CountryDetailField description={country.code} title="Código" />
-      <CountryDetailField
-        description={country.continent.name}
-        title="Contienente"
-      />
-      <CountryDetailField description={country.currency || ""} title="Moneda" />
-      <CountryDetailField
-        description={country.languages
-          .map((language) => language.name)
-          .join(" | ")}
-        title="Lenguajes"
+const CountryDetail: FC<CountryDetailProps> = ({ country }) => (
+  <div className="country-detail">
+    <div className="country-detail__header">
+      <p>
+        <span>{country.name}</span>, {country.capital}
+      </p>
+      <img
+        src={`https://countryflagsapi.com/png/${country.code}`}
+        alt={`Country-${country.name}`}
       />
     </div>
-  );
-};
+    <CountryDetailField description={country.code} title="Código" />
+    <CountryDetailField
+      description={country.continent.name}
+      title="Contienente"
+    />
+    <CountryDetailField description={country.currency || ""} title="Moneda" />
+    <CountryDetailField
+      description={country.languages
+        .map((language) => language.name)
+        .join(" | ")}
+      title="Lenguajes"
+    />
+  </div>
+);
 
 export default CountryDetail;
--- components/CountryDetail/components/CountryDetailField/CountryDetailField.tsx
+++ components/CountryDetail/components/CountryDetailField/CountryDetailField.tsx
@@ -6,13 +6,11 @@ interface CountryDetailProps {
   description: string;
 }
 
-const CountryDetailField: FC<CountryDetailProps> = ({ title, description }) => {
-  return (
-    <div className="country-field">
-      <div className="country-field__title">{title}</div>
-      <div className="country-field__desc">{description}</div>
-    </div>
-  );
-};
+const CountryDetailField: FC<CountryDetailProps> = ({ title, description }) => (
+  <div className="country-field">
+    <div className="country-field__title">{title}</div>
+    <div className="country-field__desc">{description}</div>
+  </div>
+);
 
 export default CountryDetailField;
