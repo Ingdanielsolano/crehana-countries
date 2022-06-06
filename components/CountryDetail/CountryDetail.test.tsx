d29d892fa57309ef7fd1a76d378082dc42b7a524
IngDanielSolano <ds.development.app@gmail.com>
2022-06-05 19:37:58 -0500

    test(countryDetail): setting base test

---
+++ components/CountryDetail/CountryDetail.test.tsx
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, prettyDOM, render, screen } from "@testing-library/react";
+import countryMock from "../../__mocks__/CountryMock";
+import CountryDetail from "./CountryDetail";
+
+describe("<CountryDetail />", () => {
+  test("it render", () => {
+    render(<CountryDetail country={countryMock} />);
+  });
+
+  test("Name is visible", async () => {
+    render(<CountryDetail country={countryMock} />);
+    expect(await screen.findByText("Andorra")).toBeVisible();
+  });
+
+  test("Flag is visible", async () => {
+    render(<CountryDetail country={countryMock} />);
+    expect(
+      await screen.findByAltText(`Country-${countryMock.name}`)
+    ).toBeVisible();
+  });
+});
